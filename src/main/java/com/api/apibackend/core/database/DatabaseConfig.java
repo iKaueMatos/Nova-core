6213d16f2032a4f50e40cdacf68917fbe38e4626
kaue matos <kaue178hotimail@gmail.com>
2024-01-28 22:12:13 -0300

    fix:ajustes conexão com banco de dados

--- src/main/java/com/api/apibackend/core/database/DatabaseConfig.java
+++ src/main/java/com/api/apibackend/core/database/DatabaseConfig.java
@@ -21,7 +21,7 @@ public class DatabaseConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://127.0.0.1:3308/novasoftware");
+        dataSource.setUrl("jdbc:mysql://localhost:3308/novasoftware");
         dataSource.setUsername("root");
         dataSource.setPassword("Nova");
 
